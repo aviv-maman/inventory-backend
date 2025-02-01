1999a263ff821468e199f103adc25cc67157d37b
Aviv Maman <awiwmaman@outlook.com>
2025-02-01 11:48:44 +0200

    Update storeController.ts

--- controllers/storeController.ts
+++ controllers/storeController.ts
@@ -17,31 +17,40 @@ const prepareBodyStore = helpers.catchAsync(async (req, res, next) => {
 });
 
 const updateStock = helpers.catchAsync(async (req, res, next) => {
-  const { id: productId, stock: newStockInStore } = req.body;
+  const { id: productId, stock: newStockInStore } = req.body as { id: string; stock: number };
+  const storeId = req.params.id;
 
   const product = await ProductModel.findById(productId);
   if (!product) {
     return next(new AppError('Product was not found', 404));
   }
-  const store = await StoreModel.findById(req.params.id);
+  const store = await StoreModel.findById(storeId);
   if (!store) {
     return next(new AppError('Store was not found', 404));
   }
 
-  const currentStockInStore = store.products.find((item) => item.product?.toString() === productId)?.stock || 0;
-  const stockOffset = newStockInStore - currentStockInStore;
+  const currentStockInStore = store.products.find((item) => item.product?.toString() === productId);
 
-  const updatedProduct = await product.updateOne(
-    { stock: product.stock + stockOffset },
-    { new: true, runValidators: true },
-  );
-
-  const updatedStore = await store.updateOne(
-    {
-      $set: { products: { product, stock: newStockInStore } },
-    },
-    { new: true, runValidators: true },
-  );
+  if (!currentStockInStore) {
+    //return next(new AppError(`Product with ID ${productId} is not in the store with ID ${storeId}`, 404));
+    const updatedStore = await store.updateOne(
+      {
+        $addToSet: { products: { product, stock: newStockInStore } },
+      },
+      { new: true, runValidators: true },
+    );
+    product.stock += newStockInStore;
+  } else {
+    if (newStockInStore > currentStockInStore.stock) {
+      currentStockInStore.stock += newStockInStore - currentStockInStore.stock; // Increase
+      product.stock += newStockInStore - currentStockInStore.stock;
+    } else if (newStockInStore < currentStockInStore.stock) {
+      product.stock -= currentStockInStore.stock - newStockInStore;
+      currentStockInStore.stock -= currentStockInStore.stock - newStockInStore; // Decrease
+    }
+  }
+  const updatedProduct = await product.save();
+  const updatedStore = await store.save();
 
   res.status(200).json({ success: true, data: updatedStore });
 });
