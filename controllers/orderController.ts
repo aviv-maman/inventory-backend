64d29a5382f4d8469d470ac18c7a9750d470b44e
Aviv Maman <awiwmaman@outlook.com>
2025-02-09 08:31:36 +0200

    Update orderController.ts

--- controllers/orderController.ts
+++ controllers/orderController.ts
@@ -2,10 +2,10 @@ import AppError from '../utils/AppError.ts';
 import helpers from '../utils/helpers.ts';
 
 const checkout = helpers.catchAsync(async (req, res, next) => {
-  if (!req.body.userId || req.body.localUser._id.toString() !== req.body.userId) {
+  if (!req.body.localUser._id.toString()) {
     return next(new AppError('The user ID is missing or the token belongs to someone else.', 404));
   }
-  res.status(200).json({ success: true, userId: req.body.userId });
+  res.status(200).json({ success: true, cart: req.body.cart });
 });
 
 const orderController = {
