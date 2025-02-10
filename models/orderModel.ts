22cf9b74090350a045cc8a278c8dd6759c56fb17
Aviv Maman <awiwmaman@outlook.com>
2025-02-10 10:12:41 +0200

    Update orderModel.ts

--- models/orderModel.ts
+++ models/orderModel.ts
@@ -9,17 +9,15 @@ const orderSchema = new Schema(
         product: { type: Schema.Types.ObjectId, ref: 'Product' },
         store: { type: Schema.Types.ObjectId, ref: 'Store' },
         quantity: { type: Number, default: 1 },
-        price: { type: Number, required: [true, 'An address is required'] },
+        price: { type: Number, required: [true, 'A price of a product is missing'] },
         _id: false,
       },
     ],
-    totalPrice: { type: Number, required: [true, 'A price is required'] },
+    totalPrice: { type: Number, required: [true, 'Total price is required'] },
     status: { type: String, enum: ['pending', 'in progress', 'delivered', 'canceled'], default: 'pending' },
     __v: { type: Number, select: false },
   },
-  {
-    timestamps: true,
-  },
+  { timestamps: true },
 );
 
 export const OrderModel = model('Order', orderSchema);
